d4caa3c9d382ba690b0e4a7ac7af68a367f2af7e
Adit402 <adit0016.becse24@chitkara.edu.in>
2025-01-17 14:32:55 +0530

    Solution to Input and Print an Integer

---
+++ Input and Print an Integer@@6788d5012db25efec946f939/code.c
@@ -0,0 +1,12 @@
+ n#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("You entered: %d", a);
+    return 0;
+}
