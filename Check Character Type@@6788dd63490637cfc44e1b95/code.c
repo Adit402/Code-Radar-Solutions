85ac88f0b5306695d7d15d8fdf553a78b1fa96dc
Adit402 <adit0016.becse24@chitkara.edu.in>
2025-01-26 16:53:29 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -10,7 +10,7 @@ int main() {
     if((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u')){
         printf("Vowel");
     }
-    else if((C=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U')){
+    else if((c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U')){
         printf("Vowel");
     }
     else if(c>=48 && c<=57){
