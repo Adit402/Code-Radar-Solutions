b30d4d5f12975a4a2a438131873a8de661e89a9e
Adit402 <adit0016.becse24@chitkara.edu.in>
2025-01-26 16:48:24 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -13,7 +13,7 @@ int main() {
     else if(c>=48 && c<=57){
         printf("Digit");
     }
-    else if((c>=32 && c<=47)||(c>=58 && c<=64)||(c>=91 && c<=96)||(c>=123 && c<=126)){
+    else if((c>=32 && c<=47)||(c>=58 && c<=64)||(c>=91 && c<=96)||(c>=123 && c<=126)||(c>=0 && c<=31)){
         printf("Special Character");
     }
     else{
