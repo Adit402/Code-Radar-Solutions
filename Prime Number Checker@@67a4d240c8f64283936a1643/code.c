3a658030105ed2ecaf3299c08effa759d35659a9
Adit402 <adit0016.becse24@chitkara.edu.in>
2025-02-07 22:53:11 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -4,10 +4,10 @@ void isPrime(int num){
     int i;
     for(i=2;i<num;i++){
         if(num%i==0){
-            return 0;
+            printf("0");
         }
         else{
-            return 1;
+            printf("1");
         }
     }
 }
