88c3f6c284e7c6a8bb22456adb0f847cc34d926f
Adit402 <adit0016.becse24@chitkara.edu.in>
2025-02-07 21:49:12 +0530

    Solution to Print a Pyramid Pattern with Numbers

--- Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
+++ Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
@@ -11,8 +11,8 @@ int main() {
         for(j=1;j<=n-1;j++){
             printf(" ");
         }
-        for(j=1;j<=i-1;j++){
-            printf("%d",j);
+        for(j=1;j<=2*i-1;j++){
+            printf("%d ",j);
         }
         printf("\n");
     }
