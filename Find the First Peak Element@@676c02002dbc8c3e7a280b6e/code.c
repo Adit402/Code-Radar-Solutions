f500cb4af291bf795c1e8ee221da0fa1d3c25dc6
Adit402 <adit0016.becse24@chitkara.edu.in>
2025-02-09 16:43:55 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
 
-int findFirstPeak(int arr[], int n) {
-    // Check if the first element is a peak
-    if (n == 1 || arr[0] >= arr[1]) {
-        return arr[0];
-    }
-    
-    // Check if the last element is a peak
-    if (arr[n - 1] >= arr[n - 2]) {
-        return arr[n - 1];
-    }
-
-    // Check for the first peak element in the middle of the array
-    for (int i = 1; i < n - 1; i++) {
-        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
-            return arr[i];
-        }
-    }
-
-    return -1; // This should never be reached, as there's always a peak in the array.
+char* welcome() {
+    return "Welcome to Code Radar!";
 }
 
 int main() {
-    int arr[] = {1, 3, 20, 4, 1, 0};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    int peak = findFirstPeak(arr, n);
-    
-    if (peak != -1) {
-        printf("The first peak element is: %d\n", peak);
-    } else {
-        printf("No peak element found.\n");
+    int i,n;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
     }
-    
+    for(i=1;i<n-1;i++){
+        if((a[i]>a[i+1] && a[i]>a[i-1])){
+            printf("%d",a[i]);
+            break;
+        }
+        else if(a[i]==a[i-1] && a[i]==a[i+1]){
+            printf("-1");
+        }
+        
+    }
     return 0;
 }
