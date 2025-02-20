bedce5dd51fc8313ed20d238d5751369b7f7d33c
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-02-20 17:16:06 +0530

    Solution to Streaming Service Subscription Management

---
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+struct toll{
+    char a[20];
+    char vehicle[20];
+    float d;
+};
+
+int main(){
+    int n,e=0,f=0,c=0;
+    scanf("%d", &n);
+    struct toll b[n];
+    float price,pr,pri;
+    price=0;
+    pr=0;
+    pri=0;
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%s %s %f", b[i].a, b[i].vehicle, &b[i].d);
+    }
+    for(i=0;i<n;i++){
+        if(strcmp(b[i].vehicle,"Basic")==0){
+            pr=pr+b[i].d;
+            e++;
+        }
+        else if(strcmp(b[i].vehicle,"Standard")==0){
+            pri=pri+b[i].d;
+            f++;
+        }
+        else{
+            price=price+b[i].d;
+            c++;
+        }
+    }
+    printf("Basic: %d Users, Revenue: %.1f, Standard: %d Users, Revenue: %.1f, Premium: %d Users Revenue: %.1f",e, pr,f,pri,c,price);
+}
