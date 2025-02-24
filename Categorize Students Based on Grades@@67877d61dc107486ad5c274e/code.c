c123d5a133323186e22749cde7afc36b64a84b78
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-02-24 20:16:42 +0530

    Solution to Categorize Students Based on Grades

--- Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -14,10 +14,10 @@ int main(){
         scanf("%d %s %f", &s[i].roll_number, s[i].name, &s[i].marks);
     }
     for(i=0;i<n;i++){
-        if(s[i].marks>=85){
+        if(s[i].marks>=84){
             printf("Roll Number: %d, Name: %s, Grade: A\n",s[i].roll_number,s[i].name);
         }
-        else if(s[i].marks<84 && s[i].marks>=70){
+        else if(s[i].marks<85 && s[i].marks>=70){
             printf("Roll Number: %d, Name: %s, Grade: B\n",s[i].roll_number,s[i].name);
         }
         else{
