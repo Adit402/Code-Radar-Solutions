c4232ac5b953d19fda4f894db9bb45d514d79b76
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-02 14:37:13 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,27 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int i,j,n,max, ctr=0,num=0;
+#define MAX 100000  // Adjust based on the input range
+
+int main() {
+    int n, i, maxFreq = 0, mostFreqElement = -1;
     scanf("%d", &n);
-    int a[n];
-    for(i=0;i<n;i++){
+    int a[n], freq[MAX] = {0};  // Frequency array (hash table)
+
+    // Count the frequency of each element
+    for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
+        freq[a[i]]++;  
     }
-    for(i=0;i<n;i++){
-        max=a[i];
-        for(j=0;j<n;j++){
-            if(a[j]==max){
-                ctr++;
-            }
+
+    // Find the element with the highest frequency
+    for (i = 0; i < n; i++) {
+        if (freq[a[i]] > maxFreq) {
+            maxFreq = freq[a[i]];
+            mostFreqElement = a[i];  // Store the most frequent element
+        } 
+        else if (freq[a[i]] == maxFreq && mostFreqElement != a[i]) {
+            mostFreqElement = -1;  // If there’s a tie, return -1
         }
-        if(ctr>num){
-            num=ctr;
-        }
-        else if(num==ctr){
-            num=-1;
-        }
-        ctr=0;
     }
-    printf("%d", num);
+
+    printf("%d\n", mostFreqElement);
     return 0;
 }
