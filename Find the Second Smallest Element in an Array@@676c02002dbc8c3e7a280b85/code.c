1f9dda558c288e7c8b1e5b5dce146daf6007085e
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-07 08:03:20 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,33 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n,i,num,temp,ctr=0;
+int main() {
+    int n, i, temp;
+    
     scanf("%d", &n);
     int a[n];
-    for(i=0;i<n;i++){
+
+    for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
-    for(i=0;i<n;i++){
-        if(a[i]==a[i+1]){
-            ctr++;
+
+    int allSame = 1;
+    for (i = 1; i < n; i++) {
+        if (a[i] != a[0]) {
+            allSame = 0;
+            break;
         }
     }
-    if(ctr>=n-1){
+
+    if (allSame) {
         printf("-1");
         return 0;
     }
-    for(i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
-            if(a[j]>a[j+1]){
-                num=a[j];
-                a[j]=a[j+1];
-                a[j+1]=num;
-            }
-            else if(a[j]==a[j+1]){
-                continue;
+
+    for (i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
-    temp= a[1];
-    printf("%d", temp);
+
+    int first = a[0];
+    int secondSmallest = -1;
+    
+    for (i = 1; i < n; i++) {
+        if (a[i] != first) {
+            secondSmallest = a[i];
+            break;
+        }
+    }
+
+    printf("%d", secondSmallest);
+    return 0;
 }
