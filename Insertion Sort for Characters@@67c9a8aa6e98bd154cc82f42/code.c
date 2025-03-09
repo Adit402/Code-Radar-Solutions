608cac5c9ceb866012077bcfa2d90aef708a2966
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-09 23:48:11 +0530

    Solution to Insertion Sort for Characters

---
+++ Insertion Sort for Characters@@67c9a8aa6e98bd154cc82f42/code.c
@@ -0,0 +1,18 @@
+void bubbleSort(char arr[], int n){
+    int i;
+    char temp;
+    for(i=0;i<n;i++){
+        if(arr[i]>arr[i+1]){
+            temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+}
+
+int printArray(char arr[], int n){
+    int i;
+    for(i=0;i<n;i++){
+        printf("%c ", arr[i]);
+    }
+}
