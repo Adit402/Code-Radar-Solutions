d36765cbcabda8a2c3cbc79c08408600f1cc9894
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-10 11:20:11 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 
-int main(){
-    int n,i,j,temp,k;
+int main() {
+    int n, i, j, temp;
+    
     scanf("%d", &n);
-    int a[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
-        if(a[i]<0){
-            a[i]= -a[i];
-        }
+    
+    if (n < 2) {
+        printf("Error: Need at least two numbers\n");
+        return 1;
     }
-    for(i=0;i<n;i++){
-        for(j=0;j<n-i;j++){
-            if(a[j]>a[j+1]){
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+
+    int a[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Bubble Sort (Fixed Index Issue)
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {  // Fix: Correct loop range
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
-    k=a[n-1]*a[n-2];
-    printf("%d", k);
-}   
+
+    // Compute the maximum product of two numbers
+    int max_product = a[n-1] * a[n-2];  // Max two numbers
+    int alt_product = a[0] * a[1];      // Min two numbers (could be negative * negative)
+
+    printf("%d", (max_product > alt_product) ? max_product : alt_product);
+
+    return 0;
+}
