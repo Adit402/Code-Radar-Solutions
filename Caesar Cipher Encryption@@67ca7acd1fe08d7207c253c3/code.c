7a94c44b3823dab886ac033ce3615ea555f5ebaa
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-10 10:59:16 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,7 +1,6 @@
-int caesarCipher(char message[], int shift, char encrypted[]){
+void caesarCipher(char message[], int shift, char encrypted[]){
     int i;
     for(i=0;i<'\0';i++){
         message[i]+=shift;
     }
-    return encrypted[];
 }
