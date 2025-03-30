84cb0d8bad06e0d258524c2bde4b9241ae10b9c1
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-30 11:41:12 +0530

    Solution to Digit Right-Shift Finder

--- Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
+++ Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
@@ -1,5 +1,5 @@
 int digitRightShift(int n){
-    int i,j,num,digit,ctr=0,sum;
+    int i,j=1,num,digit,ctr=0,sum;
     num=n;
     digit=n%10;
     while(n>0){
@@ -7,8 +7,8 @@ int digitRightShift(int n){
         n/=10;
     }
     for(i=1;i<ctr;i++){
-        i*=10;
+        j*=10;
     }
-    sum= (digit*i)+(num/10);
+    sum= (digit*j)+(num/10);
     return sum;
 }
