ab37028652f6c3d39c1632fe9de8b6ffea5a1841
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-30 12:10:55 +0530

    Solution to Digit Sequence Mountain Peak Finder

---
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -0,0 +1,14 @@
+int mountainPeak(int n){
+    int i,num=0;
+    while(n>0){
+        int digit=n%10;
+        if(num<digit)(
+            num=digit;
+        )
+        if(num==digit)(
+            return -1;
+            return 0;
+        )
+    }
+    return num;
+}
