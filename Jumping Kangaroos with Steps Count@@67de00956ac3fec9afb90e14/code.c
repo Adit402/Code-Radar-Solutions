e0fb43539b2e385c1ae7db3fe6c3f13d5983b2e5
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-03-30 12:47:48 +0530

    Solution to Jumping Kangaroos with Steps Count

---
+++ Jumping Kangaroos with Steps Count@@67de00956ac3fec9afb90e14/code.c
@@ -0,0 +1,10 @@
+int kangaroo(int a, int  b, int c, int d){
+    int i,j;
+    for(i=0;i<1(000;i++){
+        if((a + (b*i)) == (c + (d*i))){
+            return i;
+            return 0;
+        }
+    }
+    return -1;
+}
