4a4890ae3b8634681d5acdebf2385442988088c4
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-04-01 13:17:00 +0530

    Solution to Late Submission Penalty – Coding Contest

--- Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
+++ Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
@@ -4,7 +4,7 @@ int calculatePenalty(int date, int month, int year, int a, int b, int c){
         fine = (date-a)*10;
     }
     else if(month>b){
-        fine = 200*(month-b) + (days-a);
+        fine = 200*(month-b) + (date-a);
     }
     else if(year>c){
         fine = 5000;
