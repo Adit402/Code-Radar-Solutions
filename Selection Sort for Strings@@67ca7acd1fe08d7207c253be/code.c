1f6776a4ff0c52b5117a867add3b2715b8ae8928
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-04-08 21:41:25 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,5 +1,5 @@
 void selectionSortString(char str[]) {
-    int n = strlen(str);
+    int n = stringLength(str);
     for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
         for (int j = i + 1; j < n; j++) {
@@ -7,11 +7,18 @@ void selectionSortString(char str[]) {
                 min_idx = j;
             }
         }
-        // Swap characters
+        // Swap
         if (min_idx != i) {
             char temp = str[i];
             str[i] = str[min_idx];
             str[min_idx] = temp;
         }
     }
+}
+int stringLength(char str[]) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
 }
