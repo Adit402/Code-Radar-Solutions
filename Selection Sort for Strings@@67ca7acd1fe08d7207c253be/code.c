17fb48544a16f1df8045886dfeacc05b8a29c6a2
Adit Sharma <adit0016.becse24@chitkara.edu.in>
2025-04-08 21:46:16 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,15 +1,32 @@
-void selectionSort(char str[], int length) {
-    for (int i = 0; i < length - 1; i++) {
+void selectionSortStrings(char arr[][100], int n) {
+    for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
-        for (int j = i + 1; j < length; j++) {
-            if (str[j] < str[min_idx]) {
+
+        for (int j = i + 1; j < n; j++) {
+            int k = 0;
+            while (arr[j][k] != '\0' && arr[min_idx][k] != '\0') {
+                if (arr[j][k] < arr[min_idx][k]) {
+                    min_idx = j;
+                    break;
+                } else if (arr[j][k] > arr[min_idx][k]) {
+                    break;
+                }
+                k++;
+            }
+            if (arr[j][k] != '\0' && arr[min_idx][k] == '\0') {
                 min_idx = j;
             }
         }
+
         if (min_idx != i) {
-            char temp = str[i];
-            str[i] = str[min_idx];
-            str[min_idx] = temp;
+            for (int k = 0; k < 100; k++) {
+                char temp = arr[i][k];
+                arr[i][k] = arr[min_idx][k];
+                arr[min_idx][k] = temp;
+
+                if (arr[i][k] == '\0' && arr[min_idx][k] == '\0')
+                    break;
+            }
         }
     }
 }
